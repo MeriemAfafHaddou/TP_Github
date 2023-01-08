5b5ee5112003d1d22784beb62319ce25ab1259e5
Meriem Afaf Haddou <jm_haddou@esi.dz>
2023-01-08 11:10:00 +0100

    commit3

--- src/main/java/com/telly/controllers/UserController.java
+++ src/main/java/com/telly/controllers/UserController.java
@@ -5,4 +5,24 @@ public class UserController{
 	public String showLogout() {
 		return "loggedout";
 	}
+	public String createAccount(Model model, Principal principal) {
+		
+		model.addAttribute("user", new User());
+		
+		return "createaccount";
+	}
+	public String createUser(@Validated(FormValidationGroup.class) User user, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			return "createaccount";
+		}
+		
+		user.setAuthority("ROLE_USER");
+		user.setEnabled(true);
+
+		userService.create(user);
+		
+		return "home";
+
+	}
 }
